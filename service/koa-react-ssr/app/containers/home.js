22c59a6c56d4337e307b4918da6c2e7b84887886
clearives <clearives@gmail.com>
2019-11-26 10:35:37 +0800

    :recycle: refactor: css loader

--- service/koa-react-ssr/app/containers/home.js
+++ service/koa-react-ssr/app/containers/home.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './home.css';
 
 const Home = () => (
-  <div>
+  <div className="home">
     <h1>首页</h1>
-    <Link to="/list">跳转列表页</Link>
+    <Link to="/list">跳转列表页></Link>
   </div>
 )
 export default Home;
--- service/koa-react-ssr/build/webpack.base.conf.js
+++ service/koa-react-ssr/build/webpack.base.conf.js
@@ -19,6 +19,13 @@ module.exports = {
             ],
           }
         }
+      },
+      {
+        test: /\.css$/,
+        use: [ 
+          'style-loader', 
+          'css-loader' 
+        ]
       }
     ],
   },
