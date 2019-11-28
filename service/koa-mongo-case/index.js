0060b20da243c4b94f13a042ad01acedad03ca3c
clearives <clearives@gmail.com>
2019-11-28 10:31:28 +0800

    :construction_worker: ci: dev&& prod build

--- service/koa-mongo-case/index.js
+++ service/koa-mongo-case/index.js
@@ -1,2 +1,7 @@
-require('@babel/register');
-require('./src/app.js');
+const env = process.env.NODE_ENV || 'development';
+const src = env === 'production' ? './build/app' : './src/app';
+
+if (env === 'development') {
+  require('@babel/register');
+}
+require(src);
